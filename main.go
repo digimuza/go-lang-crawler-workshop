2e7b03c64bdf12db1c71c4d3d1d6f1def1ad3324
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-08 10:25:04 +0200

    Added go routines process

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 
-	crawledLinks := make(chan string, 100000)
-	Crawler("https://www.google.lt/search?dcr=0&source=hp&ei=kksqWuWQCuTU6ASqtbPwBQ&q=alus&oq=alus&gs_l=psy-ab.3..0l10.1938.2470.0.2698.5.4.0.0.0.0.104.301.1j2.3.0....0...1c.1.64.psy-ab..2.3.300.0..35i39k1j0i131k1.0.CkUwPC4GWZo", crawledLinks)
+	crawledLinks := make(chan string, 100)
+	go Crawler("https://www.google.lt/search?dcr=0&source=hp&ei=kksqWuWQCuTU6ASqtbPwBQ&q=alus&oq=alus&gs_l=psy-ab.3..0l10.1938.2470.0.2698.5.4.0.0.0.0.104.301.1j2.3.0....0...1c.1.64.psy-ab..2.3.300.0..35i39k1j0i131k1.0.CkUwPC4GWZo", crawledLinks)
 
 	for craw := range crawledLinks {
 		fmt.Println(craw)
@@ -25,7 +25,6 @@ func Crawler(url string, crawledLinks chan string) {
 	resp, err := http.Get(url)
 
 	if err == nil {
-		fmt.Println(resp.StatusCode)
 		linksChannel := make(chan string, 10000)
 
 		links := collectlinks.All(resp.Body)
@@ -33,7 +32,7 @@ func Crawler(url string, crawledLinks chan string) {
 		crawledLinks <- url
 
 		for _, link := range links {
-			Crawler(link, crawledLinks)
+			go Crawler(link, crawledLinks)
 		}
 
 		close(linksChannel)

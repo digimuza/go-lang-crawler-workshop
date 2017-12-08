c08db2df2f57dceaae5b2dde8b84c15743b1072b
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-08 12:07:44 +0200

    Added cmd file

--- crawle.cmd.go
+++ crawle.cmd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"./crawler"
 )
@@ -10,14 +11,22 @@ import (
 func main() {
 	baseURL := flag.String("url", "", "Crawl domain URL")
 
+	status := flag.String("status", "200,500", "Output urls with status")
+
 	flag.Parse()
 
+	statusList := strings.Split(*status, ",")
 	if *baseURL != "" {
+		fmt.Println(fmt.Sprintf("Crawling... searching links with status %s", *status))
 		crawledLinks := make(chan crawler.SiteCrawl, 100)
 		go crawler.Crawler(crawler.CrawlLink{*baseURL, *baseURL}, crawledLinks)
 
 		for craw := range crawledLinks {
-			fmt.Println(craw.CrawlLink.URL)
+
+			if contains(statusList, fmt.Sprintf("%d", craw.StatusCode)) {
+				fmt.Println(craw.CrawlLink.URL)
+			}
+
 		}
 
 		close(crawledLinks)
@@ -25,4 +34,13 @@ func main() {
 		fmt.Println("Please provide url -url=")
 	}
 
+}
+
+func contains(strings []string, e string) bool {
+	for _, a := range strings {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }

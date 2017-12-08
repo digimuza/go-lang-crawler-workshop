23781d9b34e678a26d589822f68708438a69e73c
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-08 09:27:33 +0200

    Created simple string array and loop throuth it

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/jackdanger/collectlinks"
 )
 
 func main() {
-	resp, err := http.Get("https://www.wikipedia.org/")
 
-	if err == nil {
-		links := collectlinks.All(resp.Body)
-		fmt.Println(links)
+	names := []string{"Andrius", "Dominykas", "Simas", "Mindaugas"}
+
+	for _, name := range names {
+		fmt.Println(name)
 	}
 }

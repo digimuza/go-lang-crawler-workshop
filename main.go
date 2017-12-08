8c7100d3e6fa465f55a8ff1dc2b592ec0b446cef
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-08 10:03:48 +0200

    Finished Example

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+var wg sync.WaitGroup // 1
 func main() {
 
+	// Create channel wthat will accept string and have buffer size of >4
+
+	//How many strings can hold it before consuming it
 	stringChanel := make(chan string, 4)
 
+	//Simple string array
 	names := []string{"Andrius", "Dominykas", "Simas", "Mindaugas"}
 
 	for _, name := range names {
 		stringChanel <- name
 	}
+	close(stringChanel)
 
 	for n := range stringChanel {
+		fmt.Println(len(stringChanel))
 		fmt.Println(n)
 	}
+
 }

4abd927bd3ee8ca5ee1ab711fb70e0392497f93b
andrius <andrius.mozuraitis@adeoweb.lt>
2017-12-08 11:10:17 +0200

    Added visited check

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -4,26 +4,34 @@ import (
 	"net/http"
 
 	"github.com/jackdanger/collectlinks"
+	"golang.org/x/sync/syncmap"
 )
+
+//We need to check if url is alredy visited // Normal map do not work becouse it's throws concurent read/write
+var visited = syncmap.Map{}
 
 // Crawler function that starsts crawling process
 func Crawler(url string, crawledLinks chan string) {
 
-	resp, err := http.Get(url)
+	_, ok := visited.Load(url)
+	if !ok {
+		resp, err := http.Get(url)
 
-	if err == nil {
-		linksChannel := make(chan string, 10000)
+		if err == nil {
+			visited.Store(url, true)
+			linksChannel := make(chan string, 10000)
 
-		links := collectlinks.All(resp.Body)
+			links := collectlinks.All(resp.Body)
 
-		crawledLinks <- url
+			crawledLinks <- url
 
-		for _, link := range links {
-			go Crawler(link, crawledLinks)
+			for _, link := range links {
+				go Crawler(link, crawledLinks)
+			}
+
+			close(linksChannel)
+			resp.Body.Close()
 		}
-
-		close(linksChannel)
-		resp.Body.Close()
 	}
 
 }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 
 	crawledLinks := make(chan string, 100)
-	go crawler.Crawler("https://www.google.lt/search?dcr=0&source=hp&ei=kksqWuWQCuTU6ASqtbPwBQ&q=alus&oq=alus&gs_l=psy-ab.3..0l10.1938.2470.0.2698.5.4.0.0.0.0.104.301.1j2.3.0....0...1c.1.64.psy-ab..2.3.300.0..35i39k1j0i131k1.0.CkUwPC4GWZo", crawledLinks)
+	go crawler.Crawler("https://www.knygos.lt/", crawledLinks)
 
 	for craw := range crawledLinks {
 		fmt.Println(craw)
